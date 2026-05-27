c45b07bf322be6e260df85c8c98f82cb96d1fa96
Rajashekharayya S Hiremath <rajashekharayyahiremath0@gmail.com>
2026-05-27 23:46:39 +0530

    Implemented login functionality

--- src/main/java/com/raja/salessavvy/controllers/UserController.java
+++ src/main/java/com/raja/salessavvy/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.raja.salessavvy.controllers;
+
 import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation. PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.raja.salessavvy.entities. User;
 import com.raja.salessavvy.services.UserService;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -23,7 +26,7 @@ try {
  return ResponseEntity.ok(Map.of("message", "User registered successfully", "user", registeredUser));
 }
 catch(Exception e) {
-return ResponseEntity.badRequest().body (Map.of("error", e.getMessage()));
+return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
 }
 }
 }
