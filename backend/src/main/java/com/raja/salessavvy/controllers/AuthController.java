751e674f240acdf5ef39ec23f79106f022d679db
Rajashekharayya S Hiremath <rajashekharayyahiremath0@gmail.com>
2026-09-14 17:15:58 +0530

    Update AuthController.java

--- backend/src/main/java/com/raja/salessavvy/controllers/AuthController.java
+++ backend/src/main/java/com/raja/salessavvy/controllers/AuthController.java
@@ -21,7 +21,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
+
 @RequestMapping("/api/auth")
 
 public class AuthController {
