7e2d2252de542bf0219d4d4a8b1b70b687fcabbd
Rajashekharayya S Hiremath <rajashekharayyahiremath0@gmail.com>
2026-09-14 17:17:43 +0530

    Remove CrossOrigin annotation from PaymentController
    Removed CrossOrigin annotation from PaymentController.

--- backend/src/main/java/com/raja/salessavvy/controllers/PaymentController.java
+++ backend/src/main/java/com/raja/salessavvy/controllers/PaymentController.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 @RequestMapping("/api/payment")
 public class PaymentController {
 
