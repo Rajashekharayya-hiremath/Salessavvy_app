ec2004a3682e407d6c2c4f1882adb3530d1fe27d
Rajashekharayya S Hiremath <rajashekharayyahiremath0@gmail.com>
2026-09-14 17:18:00 +0530

    Remove CrossOrigin annotation from ProductController

--- backend/src/main/java/com/raja/salessavvy/controllers/ProductController.java
+++ backend/src/main/java/com/raja/salessavvy/controllers/ProductController.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 @RequestMapping("/api/products")
 public class ProductController {
 
